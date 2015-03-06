92cc6c34a4454675fdc0c31b7420b5cea3a5a83c
Rostislav Mykhajliw <necromant2005@gmail.com>
2015-03-06 21:56:37 +0200

    groped

--- src/TweeXhprof/View/Helper/Xhprof.php
+++ src/TweeXhprof/View/Helper/Xhprof.php
@@ -42,4 +42,61 @@ class Xhprof
         $buffer .= '</table>' . PHP_EOL;
         return $buffer;
     }
+
+    public function renderGroup(array $stack)
+    {
+        $buffer = '';
+
+        $gropped = array();
+        foreach ($stack as $key => $data) {
+            list($caller, $destination) = explode('==>', $key);
+            if (!array_key_exists($caller, $gropped)) {
+                $gropped[$caller] = array();
+            }
+            $gropped[$caller][] = array($destination, $data);
+        }
+
+
+        $buffer .= '<table class="table table-hover table-profiling stupidtable">' . PHP_EOL;
+        $buffer .= '<thead>' . PHP_EOL;
+        $buffer .= '<tr>' . PHP_EOL;
+
+        $buffer .= '<th data-sort="string">Call</th>';
+        $buffer .= '<th> - </th>';
+        $buffer .= '<th data-sort="int">Count</th>';
+        $buffer .= '<th data-sort="int">Time</th>';
+        $buffer .= '<th data-sort="int">Overall</th>';
+
+        $buffer .=  PHP_EOL . '</tr>' . PHP_EOL;
+        $buffer .= '</thead>' . PHP_EOL;
+        $buffer .= '<tbody>' . PHP_EOL;
+        foreach ($gropped as $call => $items) {
+            $data = array(
+                'ct' => 0,
+                'wt' => 0,
+            );
+
+            $_buffer = '';
+            foreach ($items as list($_destination, $_data)) {
+                $_buffer .= $destination . '<br />';
+                $data['ct'] += $_data['ct'];
+                $data['wt'] += $_data['wt'];
+            }
+            $_buffer = trim($_buffer, '<br />');
+
+            $buffer .= ' <tr>' . PHP_EOL;
+
+            $buffer .= ' <td data-sort-value="' . $call . '">' . $call . '</td>';
+            $buffer .= ' <td>' . $_buffer . '</td>';
+            $buffer .= ' <td data-sort-value="' . $data['ct'] . '">' . $data['ct'] . '</td>';
+            $buffer .= ' <td data-sort-value="' . $data['wt'] . '">' . $data['wt'] . '</td>';
+            $buffer .= ' <td data-sort-value="' . ($data['ct'] * $data['wt']) . '">' . ($data['ct'] * $data['wt']) . '</td>';
+
+            $buffer .= PHP_EOL . ' </tr>' . PHP_EOL;
+        }
+        $buffer .= '</tbody>' . PHP_EOL;
+        $buffer .= '</table>' . PHP_EOL;
+
+        return $buffer;
+    }
 }
--- tests/TweeXhprof/View/Helper/XhprofTest.php
+++ tests/TweeXhprof/View/Helper/XhprofTest.php
@@ -26,4 +26,28 @@ class XhprofTest extends PHPUnit_Framework_TestCase
             $this->assertContains($key, $content);
         }
     }
+
+    public function testRenderGroup()
+    {
+        $stack = array(
+            'Zend\EventManager\SharedEventManager::attach==>array_key_exists' => array('ct' => 1, 'wt' => 1),
+            'Zend\EventManager\EventManager::setIdentifiers==>is_array' => array('ct' => 2, 'wt' => 2),
+            'Zend\EventManager\EventManager::__construct==>Zend\EventManager\EventManager::setIdentifiers' => array('ct' => 3, 'wt' => 3),
+            'Zend\EventManager\EventManager::setIdentifiers==>XXX' => array('ct' => 2, 'wt' => 2),
+            'Zend\EventManager\EventManager::__construct==>ZZZ::setIdentifiers' => array('ct' => 3, 'wt' => 3),
+        );
+
+        $match = array(
+            'Zend\EventManager\SharedEventManager',
+            'Zend\EventManager\EventManager::setIdentifiers',
+            'Zend\EventManager\EventManager::__construct',
+        );
+
+        $service = new Xhprof();
+        $content = $service->renderGroup($stack);
+        $this->assertCount(5, explode('</tr>', $content));
+        foreach ($match as $key) {
+            $this->assertContains($key, $content);
+        }
+    }
 }
